c7fe758dd51c98484717013bdc2dc73216847450
Dan Tao <daniel.tao@gmail.com>
2013-08-05 10:10:19 -0700

    store stuff here

---
+++ app.rb
@@ -0,0 +1,36 @@
+require 'json'
+require 'mongoid'
+require 'sinatra'
+
+class Doc
+  include Mongoid::Document
+  field :content, :type => String
+end
+
+configure do
+  puts "Environment: #{ENV['RACK_ENV']}"
+  Mongoid.load!(File.join(File.dirname(__FILE__), 'config', 'mongoid.yml'))
+end
+
+helpers do
+  def serve_json(data)
+    json = data.to_json
+    params['callback'] ? "#{params['callback']}(#{json})" : json
+  end
+end
+
+get '/' do
+  erb :index
+end
+
+get '/*' do |id|
+  content_type :text
+  doc = Doc.find(id)
+  serve_json(:content => doc.content)
+end
+
+post '/' do
+  content_type :json
+  doc = Doc.create(:content => params['content'])
+  serve_json(:id => doc.id)
+end
