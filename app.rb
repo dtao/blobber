e5245e2860d6b84fd088650e90ac8b3a66df8755
Dan Tao <daniel.tao@gmail.com>
2013-08-09 11:21:34 -0700

    made it possible to edit a document from the UI

--- app.rb
+++ app.rb
@@ -13,12 +13,23 @@ class Doc
 end
 
 configure do
-  enable :cross_origin
+  enable  :cross_origin
   disable :protection
+
   Mongoid.load!(File.join(File.dirname(__FILE__), 'config', 'mongoid.yml'))
+
+  Mongoid.configure do |config|
+    config.raise_not_found_error = false
+  end
 end
 
 helpers do
+  def find_doc(id)
+    doc = Doc.find(id)
+    halt serve_json(:message => 'Document does not exist') if doc.nil?
+    doc
+  end
+
   def create_doc(title, content)
     Doc.create({
       :token   => Randy.string(10),
@@ -34,7 +45,17 @@ helpers do
     doc.update_attributes(attributes)
   end
 
+  def verify_doc_editable(doc)
+    if doc.token.nil?
+      halt serve_json(:message => 'Document is not editable')
+    end
+    if doc.token != params['token']
+      halt serve_json(:message => 'Not authorized to edit document')
+    end
+  end
+
   def serve_json(data)
+    content_type :json
     json = data.to_json
     params['callback'] ? "#{params['callback']}(#{json})" : json
   end
@@ -44,9 +65,22 @@ get '/' do
   erb :index
 end
 
+get '/edit/*' do |id|
+  doc = find_doc(id)
+
+  verify_doc_editable(doc)
+
+  @id      = doc.id
+  @token   = doc.token
+  @title   = doc.title
+  @content = doc.content
+
+  erb :index
+end
+
 get '/*' do |id|
-  content_type :json
-  doc = Doc.find(id)
+  doc = find_doc(id)
+
   serve_json({
     :id      => id,
     :title   => doc.title,
@@ -56,8 +90,8 @@ get '/*' do |id|
 end
 
 post '/' do
-  content_type :json
   doc = create_doc(params['title'], params['content'])
+
   serve_json({
     :id      => doc.id,
     :token   => doc.token,
@@ -66,20 +100,12 @@ post '/' do
 end
 
 post '/*' do |id|
-  content_type :json
-  doc = Doc.find(id)
+  doc = find_doc(id)
 
-  if doc.nil?
-    halt serve_json(:message => 'Document does not exist')
-  end
-  if doc.token.nil?
-    halt serve_json(:message => 'Document is not editable')
-  end
-  if doc.token != params['token']
-    halt serve_json(:message => 'Not authorized to edit document')
-  end
+  verify_doc_editable(doc)
 
   update_doc(doc, params['title'], params['content'])
+
   serve_json({
     :id      => doc.id,
     :message => "Document updated successfully"
